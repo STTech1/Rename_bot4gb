dfbc994522e1aad158a604356fff9635887c9d5c
STTech1 <123819448+STTech1@users.noreply.github.com>
2024-04-13 19:52:37 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,39 +1,29 @@
-import asyncio
-from pyrogram import Client, compose,idle
-import os
-
-from plugins.cb_data import app as Client2
+from flask import Flask, request
+from pyrogram import Client, idle
 
 TOKEN = os.environ.get("TOKEN", "")
-
 API_ID = int(os.environ.get("API_ID", ""))
-
 API_HASH = os.environ.get("API_HASH", "")
 
-STRING = os.environ.get("STRING", "")
+bot = Client("Renamer", bot_token=TOKEN, api_id=API_ID, api_hash=API_HASH)
 
-
-bot = Client(
-
-           "Renamer",
-
-           bot_token=TOKEN,
-
-           api_id=API_ID,
-
-           api_hash=API_HASH,
-
-           plugins=dict(root='plugins'))
-           
-
-if STRING:
-    from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'STthamizhan'
 
+# Webhook route
+@app.route('/webhook', methods=['POST'])
+def handle_webhook():
+    update = request.get_json()
+    bot.handle_update(update)
+    return 'OK', 200
+
+async def run():
+    await bot.start()
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
 
 if __name__ == "__main__":
-    app.run()
+    asyncio.run(run())
+           
